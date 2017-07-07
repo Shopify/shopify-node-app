417eac19d188f587e27617e56f73ec8aea89a78c
Mallory Allen <mallory.allen@shopify.com>
2017-07-07 17:52:56 -0400

    😕  fetch based simple proxy

--- server/routes/shopifyApiProxy.js
+++ server/routes/shopifyApiProxy.js
@@ -1,35 +1,68 @@
+const {URL} = require('url');
 const proxy = require('express-http-proxy');
 const store = require('../persistentStore');
+const fetch = require('node-fetch');
 
 const ALLOWED_LIST = ['/products', '/orders'];
 
 module.exports = function shopifyApiProxy(request, response, next) {
-  const { query: { userId } } = request;
+  const {
+    query,
+    method,
+    path,
+    body,
+   } = request;
+
+  const {userId} = query;
 
   return store.getToken(userId, (err, userData) => {
     if (err || !userData) {
       return response.status(401);
     }
+
     const { shop, accessToken } = userData;
 
-    return proxy(`https://${shop}`, {
-      https: true,
-      limit: '5mb',
-      filter: function({ path }, res) {
-        const strippedPath = path.split('?')[0].split('.json')[0];
+    const strippedPath = path.split('?')[0].split('.json')[0];
 
-        return ALLOWED_LIST.some(resource => {
-          return strippedPath === resource;
-        });
-      },
-      proxyReqPathResolver({ url }) {
-        return `/admin${url}`;
-      },
-      proxyReqOptDecorator(proxyRequestOptions, sourceRequest) {
-        proxyRequestOptions.headers['X-Shopify-Access-Token'] = accessToken;
-        proxyRequestOptions.headers['content-type'] = 'application/json';
-        return proxyRequestOptions;
-      },
-    })(request, response, next);
+    const inAllowed = ALLOWED_LIST.some((resource) => {
+      return strippedPath === resource;
+    });
+
+    if (!inAllowed) {
+      return response.status(403);
+    }
+
+    fetchWithParams(`https://${shop}/admin/${path}`, {
+        method,
+        body,
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Shopify-Access-Token': accessToken,
+        },
+    })
+    .then(remoteResponse => {
+      remoteResponse
+        .json()
+        .then(responseBody => {
+          response
+            .status(remoteResponse.status)
+            .send(responseBody);
+        })
+    })
+
   });
+};
+
+function fetchWithParams(url, fetchOpts, params = {}) {
+  const parsedUrl = new URL(url)
+
+  parsedUrl.searchParams.delete('userId');
+
+  Object
+    .keys(params)
+    .forEach(key => {
+      parsedUrl.searchParams.append(key, params[key])
+    });
+
+  return fetch(parsedUrl.href, fetchOpts);
 };
